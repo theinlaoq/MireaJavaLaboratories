ee0f9b0ae86921bfbe02828221e627c83bf676f9
Alexey Podkolzin <podkalex00309@gmail.com>
2023-09-10 19:00:05 +0300

    + paru lab

---
+++ src/ru/mirea/lab3/Obertki/Tester.java
@@ -0,0 +1,14 @@
+package ru.mirea.lab3.Obertki;
+
+public class Tester {
+    public static void main(String[] args) {
+        String a = "123.5332";
+        Double b = Double.valueOf(a);
+        Double c = Double.parseDouble(a);
+        System.out.println(b);
+        System.out.println(c);
+        Double d = 121.1;
+        
+
+    }
+}
