aab9e961d168da9855f8fe404f28b270d91c073a
admin <piliya.t.s@gmail.com>
2023-09-19 10:15:25 +0300

    ex10

---
+++ src/ru/mirea/lab6/ex10/Monitor.java
@@ -0,0 +1,13 @@
+package ru.mirea.lab6.ex10;
+
+public class Monitor {
+    private String type;
+
+    public Monitor(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
